37f60a6f0a1cf839a41d21bcce1e26c60fbfb4ec
Ahmed Zidan <ahmadibrahim.zidan@gmail.com>
2026-01-10 19:08:46 +0200

    1st

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Header from "@/components/header";
 import Hero from "@/components/hero";
 import Services from "@/components/services";
 import WhyUs from "@/components/why-us";
-import Review from "@/components/review";
+/* import Review from "@/components/review"; */
 import CallToAction from "@/components/call-to-action";
 
 import Footer from "@/components/footer";
@@ -18,7 +18,7 @@ export default function Home() {
       <Hero />
       <Services />
       <WhyUs />
-      <Review />
+     {/*  <Review /> */}
       <CallToAction />
     
 
